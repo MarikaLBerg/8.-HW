757dcf31cf14dd8e44b7313ed63f2529a1368de0
MarikaL <marinadvkusny@gmail.com>
2025-04-01 19:36:54 +0300

    added items

--- modules/content.js
+++ modules/content.js
@@ -1,5 +1,6 @@
 import Element from "../constructor/Element.js";
 
+// Данные для карточек
 const cardData = [
     'Card 1',
     'Card 2',
@@ -8,19 +9,24 @@ const cardData = [
     'Card 5',
 ];
 
-const cards = cardData.map(el => new Element('div', 'card', el).get());
+// Создание карточек
+const cards = cardData.map(item => new Element('div', 'card', item).get());
 
+// Данные для новых карточек
 const newCardData = [
     'New Card A',
     'New Card B'
 ];
 
-const newCards = newCardData.map(el => new Element('div', 'small-card', el).get());
+// Создание новых карточек
+const newCards = newCardData.map(item => new Element('div', 'small-card', item).get());
 
-// Вызов метода get() для получения элементов
+// Создание секции с карточками
 const cardSection = new Element('div', 'card-section', cards).get();
 const newCardSection = new Element('div', 'new-card-section', newCards).get();
 
+// Создание основного контента
 const content = new Element('div', 'content', [cardSection, newCardSection]).get();
 
+// Экспорт основного контента
 export default content;
