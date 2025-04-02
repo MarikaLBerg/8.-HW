961b0fab39eab53dd8b2437f5d19ddb4c46c9851
MarikaL <marinadvkusny@gmail.com>
2025-04-02 20:35:52 +0300

    small card to be added

--- modules/content.js
+++ modules/content.js
@@ -15,7 +15,8 @@ const cards = cardData.map(item => new Element('div', 'card', item).get());
 // Данные для новых карточек
 const newCardData = [
     'New Card A',
-    'New Card B'
+    'New Card B',
+    'New Card C'
 ];
 
 // Создание новых карточек
@@ -26,7 +27,7 @@ const cardSection = new Element('div', 'card-section', cards).get();
 const newCardSection = new Element('div', 'new-card-section', newCards).get();
 
 // Создание основного контента
-const content = new Element('div', 'content', [cardSection, newCardSection]).get();
+const content = new Element('div', 'content', [cardSection, newCardSection]);
 
 // Экспорт основного контента
 export default content;
